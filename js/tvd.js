ee281e02d937fef689c094465ab23879c44013b9
lamasagar <sagar.lama@globaly.io>
2021-01-22 23:15:14 +0545

    Mobile support

--- js/tvd.js
+++ js/tvd.js
@@ -1,12 +1,29 @@
 // var $ = jQuery.noConflict();
 // $(document).ready(function() {
+const isMobile = () => {
+  if (
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    screen.width <= 699
+  ) {
+    return true;
+  }
+};
+
 const toggleTvdModel = () => {
   const tvd_model = document.getElementById("tvd-model");
   const tvd_btn = document.getElementById("tvd");
+  const mob = document.getElementById("mob");
+  const pc = document.getElementById("pc");
   let hidden =
     tvd_model.style.display === "none" || tvd_model.style.display === "";
   if (hidden) {
     tvd_model.style.display = "flex";
+    if (isMobile()) {
+      mob.style.display = "flex";
+    } else {
+      pc.style.display = " flex";
+    }
     tvd_btn.style.display = "none";
     document.body.style.overflow = "hidden";
   } else {
